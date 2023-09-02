46cd11e89c3391f5e2ffd55db907e189d06aef37
JagerAttila <kitalalt@gmail.com>
2023-09-02 14:59:15 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+interface Vmi {
+  key:string;
+  text:string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'TemplateMuveletek';
+  // tomb =[]
+  tomb =["alma","cseresznye", 123, "nyuszika"];
+  
+  
+
+  obj= [
+    {"name":"Kati", "age":22},
+    {"name":"Attila", "age":32},
+    {"name":"Ingrid", "age":31},
+  ]
+
+  oszlopok:string[]=["name", "age"]
+
+  ujEmber:any={}
+
+  emberTorlese(i:number){
+    this.obj.splice(i,1)
+  }
+  ujEmberFelvetele(){
+    this.obj.push(this.ujEmber);
+    this.ujEmber={}
+  }
 }
